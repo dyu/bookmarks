9bb5e7438e5b2274d1305783232aa6e2b0d4a987
dyu <david.yu.ftw@gmail.com>
2017-04-27 01:05:03 +0800

    import impl

--- bookmarks-importer/import.sh
+++ bookmarks-importer/import.sh
@@ -3,6 +3,9 @@
 [ $# -lt 1 ] && echo '1st arg (in file) and 2nd arg (out dir) is required.' && exit 0
 
 JAR=target/bookmarks-importer-jarjar.jar
+CLI_DIR=../bookmarks-cli
+CLI_JAR=target/bookmarks-cli-jarjar.jar
+CLI_EXEC="java -Dcli.separator=! -Ddump.with_seq_key=true -jar $CLI_DIR/$CLI_JAR"
 
 jarjar() {
   rm -f target/*.jar
@@ -32,6 +35,13 @@ mkdir -p $OUT_DIR
 STATUS_FILE=$OUT_DIR/status.txt
 
 [ -e $JAR ] || jarjar
+[ -e $CLI_DIR/$CLI_JAR ] || { cd $CLI_DIR; jarjar; cd - > /dev/null; }
 
-java -jar $JAR $IN_FILE $OUT_DIR $@ 2> $STATUS_FILE && echo "Successful. [$STATUS_FILE]"
+java -jar $JAR $IN_FILE $OUT_DIR $@ 2> $STATUS_FILE && \
+    $CLI_EXEC dump -i $OUT_DIR -o $OUT_DIR/dump user && \
+    $CLI_EXEC load -sev -i $OUT_DIR/dump -o $OUT_DIR/load user && \
+    $CLI_EXEC index -i $OUT_DIR/load -o $OUT_DIR/index user && \
+    $CLI_EXEC pipe -i $OUT_DIR/index -o $OUT_DIR/pipe user && \
+    rm -r $OUT_DIR/load && rm -r $OUT_DIR/index && \
+    echo "Successful. [$STATUS_FILE]"
 
