f3397dce001ddc1be14917f38401898dc478db7d
dyu <david.yu.ftw@gmail.com>
2017-06-19 10:25:47 +0800

    standalone linux dist

---
+++ scripts/s-archive.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+[ -e scripts ] || { echo 'Execute this script from root dir.'; exit 1; }
+
+TAR_FILE=target/bookmarks-linux-standalone-x64.tar.gz
+
+echo "========== tar.gz"
+rm -f $TAR_FILE
+echo '#!/bin/sh' > start.sh && tail --lines=+4 scripts/s-start.sh >> start.sh && chmod +x start.sh && \
+tar -cvzf $TAR_FILE start.sh target/hprotostuffdb-rjre target/jre/*/ -T scripts/files.txt
+rm start.sh
---
+++ scripts/s-start.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+[ -e scripts ] || { echo 'Execute this script from root dir.'; exit 1; }
+[ -e target/jre ] || { echo 'The target/jre dir is missing.'; exit 1; }
+
+BIN=target/hprotostuffdb-rjre
+DATA_DIR=target/data/main
+JAR=bookmarks-all/target/bookmarks-all-jarjar.jar
+ASSETS=-Dprotostuffdb.assets_dir=bookmarks-ts
+ARGS=$(cat ARGS.txt)
+PORT=$(cat PORT.txt)
+
+$BIN $PORT bookmarks-ts/g/user/UserServices.json $ARGS $ASSETS -Djava.class.path=$JAR bookmarks.all.Main
+
