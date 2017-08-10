7663459dfd54281de46613038ed75aeea848f0ff
dyu <david.yu.ftw@gmail.com>
2017-08-10 23:48:42 +0800

    ordering

--- bookmarks-ts/src/user/BookmarkEntryBase.ts
+++ bookmarks-ts/src/user/BookmarkEntryBase.ts
@@ -8,64 +8,6 @@ import * as form from 'coreds/lib/form'
 import { filters, MAX_TAGS } from './context'
 import { user } from '../../g/user/'
 const $ = user.BookmarkEntry
-const Tag = user.BookmarkTag
-
-export const enum TagState {
-    REMOVE = 32
-}
-
-export const Item = {
-    name: 'Item', data() { return {} },
-    props: {
-         pojo: { type: Object, required: true },
-         detail_id: { type: String, required: true }
-    }, 
-    filters,
-    template: /**/`
-<li ${ui.pi_attrs}>
-  <div class="content right floated">
-    ${ui.icon_toggle($.$.active, 32, 'circle', $.$d[$.$.active].$n)}
-  </div>
-  <div class="content right floated timeago">${ui.icon_timeago}</div>
-  <div class="content right floated timeago hide-pp hide-tp"><i class="icon calendar"></i>{{ pojo['${$.$.date}'] | ymd }}</div>
-  <div :class="'content' + (pojo['${$.$.active}'] ? '' : ' line-through')">
-    <a :style="pojo['${$.$.normalized}'] | word_wrap" :href="pojo['${$.$.url}'] | href" target="_blank" rel="noreferrer">
-      <span v-show="pojo['${$.$.www}']">www.</span>{{ pojo['${$.$.normalized}'] }}
-    </a>
-    <div v-text="pojo['${$.$.title}']"></div>
-  </div>
-  <div :class="'tags inline' + (!(pojo._.state & ${PojoState.UPDATE}) ? ' noop' : '')">
-    <span v-for="(tag, idx) of pojo['${$.M.$.tags}']" class="ui label" :style="{ color: '#' + tag['${Tag.$.color}'] }">
-      {{ tag['${Tag.$.name}'] }}
-      <i class="icon action close" @click="$emit('rm_tag', idx)"></i>
-    </span>
-  </div>
-  ${ui.pi_msg}
-  <div class="detail-p" v-show="pojo._.state & ${PojoState.UPDATE}" v-append:$detail_id="pojo._.state & ${PojoState.UPDATE}"></div>
-</li>
-            `/**/
-}
-
-export function $list(detail_id: string) {
-    return /**/`
-<ul class="ui small divided selection list">
-  <item v-for="pojo of pager.array" :pojo="pojo" :detail_id="'${detail_id}'"
-      @toggle="toggle" @rm_tag="tag_upd$$rm" />
-</ul>
-<div style="display:none">
-  <div id="${detail_id}" class="detail">
-    <hr />
-    <div class="field suggest" v-clear="tag_upd">
-      <i class="icon plus"></i>
-      <input placeholder="Tag" type="text" ref="tag_upd"
-          :disabled="pupdate.tag_count === ${MAX_TAGS} || 0 !== (tag_upd.state & ${PojoState.LOADING})"
-          v-suggest="{ pojo: tag_upd, field: 'f', fetch: suggest, onSelect: tag_upd$$, vk: '${user.BookmarkTag.$.id}' }" />
-    </div>
-    ${ui.form('pupdate', $.$d, null)}
-  </div>
-</div>
-    `/**/
-}
 
 export abstract class View {
     pager: Pager
@@ -157,4 +99,58 @@ export abstract class View {
         this['$refs'].tag_upd.value = ''
         return false
     }
+}
+
+const Tag = user.BookmarkTag
+export const Item = {
+    name: 'Item', data() { return {} },
+    props: {
+         pojo: { type: Object, required: true },
+         detail_id: { type: String, required: true }
+    }, 
+    filters,
+    template: /**/`
+<li ${ui.pi_attrs}>
+  <div class="content right floated">
+    ${ui.icon_toggle($.$.active, 32, 'circle', $.$d[$.$.active].$n)}
+  </div>
+  <div class="content right floated timeago">${ui.icon_timeago}</div>
+  <div class="content right floated timeago hide-pp hide-tp"><i class="icon calendar"></i>{{ pojo['${$.$.date}'] | ymd }}</div>
+  <div :class="'content' + (pojo['${$.$.active}'] ? '' : ' line-through')">
+    <a :style="pojo['${$.$.normalized}'] | word_wrap" :href="pojo['${$.$.url}'] | href" target="_blank" rel="noreferrer">
+      <span v-show="pojo['${$.$.www}']">www.</span>{{ pojo['${$.$.normalized}'] }}
+    </a>
+    <div v-text="pojo['${$.$.title}']"></div>
+  </div>
+  <div :class="'tags inline' + (!(pojo._.state & ${PojoState.UPDATE}) ? ' noop' : '')">
+    <span v-for="(tag, idx) of pojo['${$.M.$.tags}']" class="ui label" :style="{ color: '#' + tag['${Tag.$.color}'] }">
+      {{ tag['${Tag.$.name}'] }}
+      <i class="icon action close" @click="$emit('rm_tag', idx)"></i>
+    </span>
+  </div>
+  ${ui.pi_msg}
+  <div class="detail-p" v-show="pojo._.state & ${PojoState.UPDATE}" v-append:$detail_id="pojo._.state & ${PojoState.UPDATE}"></div>
+</li>
+            `/**/
+}
+
+export function $list(detail_id: string) {
+    return /**/`
+<ul class="ui small divided selection list">
+  <item v-for="pojo of pager.array" :pojo="pojo" :detail_id="'${detail_id}'"
+      @toggle="toggle" @rm_tag="tag_upd$$rm" />
+</ul>
+<div style="display:none">
+  <div id="${detail_id}" class="detail">
+    <hr />
+    <div class="field suggest" v-clear="tag_upd">
+      <i class="icon plus"></i>
+      <input placeholder="Tag" type="text" ref="tag_upd"
+          :disabled="pupdate.tag_count === ${MAX_TAGS} || 0 !== (tag_upd.state & ${PojoState.LOADING})"
+          v-suggest="{ pojo: tag_upd, field: 'f', fetch: suggest, onSelect: tag_upd$$, vk: '${user.BookmarkTag.$.id}' }" />
+    </div>
+    ${ui.form('pupdate', $.$d, null)}
+  </div>
+</div>
+    `/**/
 }
