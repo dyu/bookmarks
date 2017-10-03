7a996e874115b4654b6448475f6344e8c99b7026
dyu <david.yu.ftw@gmail.com>
2017-10-03 17:52:50 +0800

    backup script

---
+++ rslave/backup.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# locate
+if [ ! -n "$BASH_SOURCE" ]; then
+    SCRIPT_DIR=`dirname "$(readlink -f "$0")"`
+else
+    F=$BASH_SOURCE
+    while [ -h "$F" ]; do
+        F="$(readlink "$F")"
+    done
+    SCRIPT_DIR=`dirname "$F"`
+fi
+
+cd $SCRIPT_DIR
+
+BIN=/opt/protostuffdb/bin/ws-cli-backup
+
+MASTER_PORT=$(cat ../PORT.txt)
+
+CONNECT_URL="ws://127.0.0.1:$MASTER_PORT/bookmark123456781234567812345678"
+
+DATE=$(date)
+BACKUP_NAME=$(date -d "$DATE" +%Y-%m-%d_%H-%M-%S)
+
+$BIN $CONNECT_URL $BACKUP_NAME $@
