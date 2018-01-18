0a89c7c7be0dc53124b7ca73e82cd4bcc2257d80
dyu <david.yu.ftw@gmail.com>
2018-01-18 15:52:28 +0800

    extension

---
+++ bookmarks-ts/extension/popup/index.js
@@ -0,0 +1,33 @@
+/* initialise variables */
+
+var inputUrl = document.querySelector('.new-bkm input.url');
+var inputTitle = document.querySelector('.new-bkm input.title');
+var inputBody = document.querySelector('.new-bkm textarea');
+var noteContainer = document.querySelector('.note-container');
+var addBtn = document.querySelector('.add');
+
+/*  add event listeners to buttons */
+
+addBtn.addEventListener('click', add);
+
+/* generic error handler */
+function onError(error) {
+  console.log(error);
+}
+
+initialize();
+
+function setInfo(tabs) {
+  var tab = tabs[0]
+  inputUrl.value = tab.url
+  inputTitle.value = tab.title
+  inputBody.value = toKV(tab)
+}
+
+function initialize() {
+  browser.tabs.query({ active: true, currentWindow: true }, setInfo);
+}
+
+function add() {
+  
+}
